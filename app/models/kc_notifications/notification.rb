9bdb0cb396385f6d88bf75d1515f035ed9950af3
libochao <522427432@qq.com>
2015-10-23 16:29:54 +0800

    改成hash

--- app/models/kc_notifications/notification.rb
+++ app/models/kc_notifications/notification.rb
@@ -1,16 +1,10 @@
 module KcNotifications
   class Notification
-    class << self
-      def with_kind(query_kind)
-        scope :query_kind, -> { where(kind: query_kind) }
-        self.query_kind
-      end
-    end
-
     include Mongoid::Document
     include Mongoid::Timestamps
     field :kind,  :type => String
-    field :info, :type => String
+    field :info, :type => Hash
+    scope :with_kind, ->(query_kind){ where(kind: query_kind) }
     belongs_to :user,:class_name => KcNotifications.user_class
   end
 end
--- sample/spec/models/notification_spec.rb
+++ sample/spec/models/notification_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe KcNotifications::Notification, type: :model do
   describe '使用方法 user.notifications.create' do
     it{
       user_tom = create(:user)
-      info = '早上好'
+      info = {:a => '早上好'}
       user_tom.notifications.create(:kind => "system", :info => info)
       expect(user_tom.notifications.count).to eq(1)
       expect(user_tom.notifications.last.kind).to eq("system")
@@ -13,8 +13,8 @@ RSpec.describe KcNotifications::Notification, type: :model do
   describe '使用方法scope' do
     it 'user.notifications.with_kind'do
       user_tom = create(:user)
-      info1 = '系统消息'
-      info2 = '邮件消息'
+      info1 = {'a' => '系统消息'}
+      info2 = {'b' => '邮件消息'}
       user_tom.notifications.create(:kind => "system", :info => info1)
       expect(user_tom.notifications.with_kind("system").first.info).to eq(info1)
       user_tom.notifications.create(:kind => "message", :info => info2)
